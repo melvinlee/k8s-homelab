aad24e66abb061c6c51326896b18abbc4e61aadd
melvinlee <melvinlee@hotmail.com>
2025-02-24 23:15:34 +0800

    feat: refactor event hook for better handling of Metallb CR deployment

--- helmfile/releases/metallb/config.sh
+++ helmfile/releases/metallb/config.sh
@@ -2,18 +2,26 @@
 
 function wait_for_crds() {
     local crds=(
-        "ipaddresspools" "ipaddresspools"
+        "ipaddresspools" "l2advertisements"
     )
 
     for crd in "${crds[@]}"; do
         until kubectl get crd "${crd}.metallb.io" &>/dev/null; do
-            echo "Waiting for CRD $crd to be available..."
+            echo "Waiting for CRD ${crd} to be available..."
             sleep 10
         done
     done
 }
 
+function apply_config() {
+    sleep 20
+    echo "Applying Metallb config"
+    kubectl apply -f resources/pool.yaml
+}
+
 function main() {
     wait_for_crds
-    # apply_config
+    apply_config
 }
+
+main "$@"
